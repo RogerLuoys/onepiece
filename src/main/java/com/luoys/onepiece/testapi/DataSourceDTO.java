c8e1b2717eb56015c010e981b3bed218cef7decf
luoys <luoys@servyou.com.cn>
2021-07-13 20:04:41 +0800

    更新

---
+++ src/main/java/com/luoys/onepiece/testapi/DataSourceDTO.java
@@ -0,0 +1,16 @@
+package com.luoys.onepiece.testapi;
+
+import lombok.Data;
+
+@Data
+public class DataSourceDTO {
+
+    private String driver;
+
+    private String url;
+
+    private String userName;
+
+    private String password;
+
+}
--- src/test/java/com/luoys/onepiece/StringTest.java
+++ src/test/java/com/luoys/onepiece/StringTest.java
@@ -1,6 +1,11 @@
 package com.luoys.onepiece;
 
+import com.alibaba.fastjson.JSON;
+import com.luoys.onepiece.testapi.DataSourceDTO;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class StringTest {
 
@@ -10,4 +15,16 @@ public class StringTest {
         String t2 = t1.replace("1", "$$$");
         System.out.println(t1 +"\n"+t2);
     }
+
+    @Test
+    public void test2() {
+        String t1 = "{\"driver\":\"test driver\",\"password\":\"test password\",\"url\":\"test url\",\"userName\":\"test user name\"} &&& {\"sql\":\"update device set description='auto' where id=1;\",\"type\":3}";
+        List<String> sqlList = Arrays.asList(t1.split(" &&& "));
+        String t2 = sqlList.get(0);
+        System.out.println(t2);
+        DataSourceDTO dataSourceDTO = JSON.parseObject(t2, DataSourceDTO.class);
+        System.out.println(sqlList);
+        System.out.println(dataSourceDTO);
+    }
+
 }
